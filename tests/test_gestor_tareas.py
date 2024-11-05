69f51d19831cf5a1d8d023637fd2c9972d78a0e4
labf402 <75244564@continental.edu.pe>
2024-11-04 20:01:10 -0500

    se agrego Test_agregar tarea sin titulo (rojo)

--- tests/test_gestor_tareas.py
+++ tests/test_gestor_tareas.py
@@ -12,6 +12,11 @@ def test_agregar_tarea(self):
     self.assertEqual(self.gestor.tareas[0].titulo, "Tarea 1")
     self.assertEqual(self.gestor.tareas[0].descripcion, "Descripción de la tarea 1")
 
+def test_agregar_tarea_sin_titulo(self):
+    with self.assertRaises(ValueError):
+        self.gestor.agregar_tarea("", "Descripción")
+
+
 
 
 if __name__ == "__main__":
